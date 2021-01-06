6ef48fb5abddd4d6af267ef5a103527d779bbae4
foram-github <foram.dv133@gmail.com>
2021-01-06 05:59:02 -0800

    1st commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+
+/*pre loader*/
+$(window).on('load', function() {
+    $('.preloader').fadeOut();
+});
+
+/*Toggle Class*/
+(function ($) {
+    $(".toggle-password").on('click', function () {
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+})(jQuery);
+
+
+/*AOS Animation*/
+
+AOS.init();
+
+
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function(){
+        e.target.classList.remove('animate');
+    },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+}
